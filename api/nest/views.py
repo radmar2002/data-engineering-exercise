ff0811cd3ff11bc2c8efe906826ce90a585c4dad
Marius Radu <radu_marius_florin@yahoo.com>
2021-05-19 23:56:00 +0300

    put nest list logic as RES API point -01

--- api/nest/views.py
+++ api/nest/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+import pandas as pd
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import authentication, permissions
 from rest_framework.permissions import IsAuthenticated
 
 from .throttles import LimitedRateThrottle, BurstRateThrottle
+from collections import defaultdict
+
+
+def list_nester(input_df, n_levels):
+
+    gr_els = list(n_levels)
+    n_levels.append("amount")
+    df_reduced = input_df[n_levels]
+    grouped_reduced = df_reduced.groupby(gr_els).sum()
+    #grouped_reduced_toparse = grouped_reduced.to_dict(orient='index')
+
+    results = defaultdict(lambda: defaultdict(dict))
+
+    for index, value in grouped_reduced.itertuples():
+        for i, key in enumerate(index):
+            if i == 0:
+                nested = results[key]
+            elif i == len(index) - 1:
+                nested[key] = [{'amount': value}]
+            else:
+                nested = nested[key]
+
+    res = json.dumps(results, indent=4)
+    return res
 
 
 class NestedAmountsView(APIView):
@@ -15,8 +42,11 @@ class NestedAmountsView(APIView):
     throttle_classes = [LimitedRateThrottle]
 
     def post(self, request, format=None):
-        data = request.data
-        response_dict = {"nested_amounts": data}
+
+        inputdata = request.data
+        idata = pd.DataFrame(inputdata)
+        nl = ["country", "city", "currency"]
+        response_dict = json.loads(list_nester(idata, nl))
         return Response(response_dict, status=200)
 
     def get(self, request, format=None):
