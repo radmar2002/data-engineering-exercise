58b989c77a404ade65520c50895104640f0746d0
Marius Radu <radu_marius_florin@yahoo.com>
2021-05-19 22:37:43 +0300

    prepare task 1 code -02

--- tests.py
+++ tests.py
@@ -1,35 +1,63 @@
 """
 Use Python unittest for nest.py script
 """
-import random
+import json
 import unittest
+import pandas as pd
+
+from nest import list_nester
+
+
+def dict_compare(d1, d2):
+    d1_keys = set(d1.keys())
+    d2_keys = set(d2.keys())
+    shared_keys = d1_keys.intersection(d2_keys)
+    added = d1_keys - d2_keys
+    removed = d2_keys - d1_keys
+    modified = {o: (d1[o], d2[o]) for o in shared_keys if d1[o] != d2[o]}
+    same = set(o for o in shared_keys if d1[o] == d2[o])
+    return added, removed, modified, same
 
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
-        self.seq = list(range(10))
+        self.inputfile = pd.read_json('test_input.json')
+        self.input = pd.read_json('input.json')
+        self.expected = pd.read_json('expected_output.json')
 
-    def test_shuffle(self):
-        """ make sure the shuffled sequence does not lose any elements """
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, list(range(10)))
+        # inp = self.input.to_json()
+        # exp = self.expected.to_json()
+        # print(inp)
+        # print(exp)
 
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1, 2, 3))
+    def test_good_result(self):
+        """
+        check if expected VS. delivered jsons are:
+            added, removed, modified, same
+        """
+        added, removed, modified, same = dict_compare(
+            self.input.to_dict(), self.expected.to_dict())
+        self.assertEqual(len(modified), 0)
 
-    def test_choice(self):
-        """ test a choice """
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
+    def test_list_nester_type(self):
+        # should raise an exception if list nester return_type is not string
+        resnl = list_nester(self.inputfile, ["country", "city"])
+        self.assertIs(type(resnl), str)
 
-    def test_sample(self):
-        """ test that an exception is raised """
-        with self.assertRaises(ValueError):
-            random.sample(self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
+    def test_four_columns_mock(self):
+
+        # should raise an exception for if there are no
+        self.assertEqual(len(self.inputfile.columns), 4)
+
+    def test_amount(self):
+        """ test amount column presence """
+        element = "amount"
+        self.assertTrue(element in list(self.inputfile.columns))
+
+        """ test ammounts column presence """
+        element = "ammounts"
+        self.assertFalse(element in list(self.inputfile.columns))
 
 
 if __name__ == '__main__':
