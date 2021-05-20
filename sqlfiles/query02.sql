55ee6e556f47ed24fa904e0b327d4ce1b9daeb01
Marius Radu <radu_marius_florin@yahoo.com>
2021-05-20 18:23:01 +0300

    add sql query for sql task point2 -01

--- sqlfiles/query02.sql
+++ sqlfiles/query02.sql
@@ -2,15 +2,25 @@
 --SELECT * FROM transactions LIMIT 20;
 
 /*
-1. Write down a query that gives us a breakdown of spend in GBP by each user. 
-Use the exchange rate with the largest timestamp. 
+Write down the same query, but this time, 
+use the latest exchange rate smaller or equal than the transaction timestamp. 
+Solution should have the two columns: 
+user_id, total_spent_gbp, ordered by user_id
 */
 
-WITH exratestable AS (
-	SELECT from_currency AS fc
-		   ,MAX(ts) AS lts
-	FROM exchange_rates
-	GROUP BY from_currency
+WITH closest_rate_by_transaction AS (
+	SELECT 
+		tr.*
+		,er.*
+		,er.ts as lts,
+		-- calc. distance between transactions timestamp and exchange rates timestamp
+		row_number() over (partition by er.from_currency order by er.ts desc) as distance 
+	FROM transactions AS tr
+	INNER JOIN exchange_rates AS er on er.from_currency = tr.currency
+	WHERE er.ts < tr.ts
+), exratestable AS (
+SELECT currency AS fc, lts FROM closest_rate_by_transaction
+WHERE distance = 1 -- shortest distance
 ), lefexratestable AS (
 	SELECT * FROM exratestable 
 	LEFT JOIN exchange_rates 
@@ -29,8 +39,7 @@ WITH exratestable AS (
 			prodconv as er,  
 			ROUND((amount*prodconv),2) AS spent
 	FROM gbptransactions
-) 
-SELECT user_id
+) SELECT user_id
 		,SUM(spent) AS total_spent_gbp 
 FROM calctable
 GROUP BY user_id
